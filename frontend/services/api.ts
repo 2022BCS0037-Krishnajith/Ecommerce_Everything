c3570e0b3821614f061f78a74cd0f47a018d3c47
Krishnajith <krishnajith@Krishnajiths-Mac-mini.local>
2026-05-21 14:46:43 +0530

    Final api.ts fix

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,16 +1,20 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL
 })
 
 API.interceptors.request.use((req) => {
 
-  const token = localStorage.getItem("token")
+  const token =
+    localStorage.getItem("token")
 
   if (token) {
 
-    req.headers.Authorization = `Bearer ${token}`
+    req.headers.Authorization =
+      `Bearer ${token}`
   }
 
   return req
