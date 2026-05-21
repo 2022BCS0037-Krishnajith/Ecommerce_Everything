a39f97113dd7dfb5df4ade0469e3411e042cfbe0
Krishnajith <krishnajith@Krishnajiths-Mac-mini.local>
2026-05-21 15:50:14 +0530

    Removed temporary admin route

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,29 +28,6 @@ app.include_router(product.router)
 app.include_router(auth.router)
 app.include_router(cart.router)
 app.include_router(order.router)
-@app.get("/make-admin")
-def make_admin(email: str):
-
-    db = SessionLocal()
-
-    user = db.query(User).filter(
-        User.email == email
-    ).first()
-
-    if not user:
-
-        return {
-            "message": "User not found"
-        }
-
-    user.is_admin = True
-
-    db.commit()
-
-    return {
-        "message": "Admin updated"
-    }
-    
 @app.get("/")
 def home():
     return {"message": "E-commerce Backend Running"}
